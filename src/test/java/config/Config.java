1a14e012daf5869d7d56189ef005fb498c13e7a1
xhurma <dimagu.doc@gmail.com>
2025-02-16 18:27:47 +0700

    Add Config class Config.java contains API endpoints

--- src/test/java/config/Config.java
+++ src/test/java/config/Config.java
@@ -1,4 +1,10 @@
 package config;
 
 public class Config {
+    public static final String BASE_URL = "https://qa-internship.avito.com/api/1";
+
+    public static final String CREATE_ITEM = Config.BASE_URL + "/item";
+    public static final String GET_ITEM_BY_ID = Config.BASE_URL + "/item/{id}";
+    public static final String GET_ITEMS_BY_SELLER = Config.BASE_URL + "/{sellerID}/item";
+    public static final String GET_STATISTICS_BY_ID = Config.BASE_URL + "/statistic/{id}";
 }
